ab81d2d3735759eddafe465df546dabe4ea0fc84
IceVenOm98 <IceVenOm98@gmail.com>
2020-09-24 21:11:09 +0300

    decompose GetCountry method to three levels: -getting string from API; -editing string for each API; -deserializing string as JSON to object.

--- CountryInfo/JSONAPIConnecter.cs
+++ CountryInfo/JSONAPIConnecter.cs
@@ -7,39 +7,33 @@ namespace CountryInfo
     internal class JSONAPIConnecter : Interfaces.IAPIConnecter
     {
         public int mode { get; set; }
-        public Country GetCountry(String url)
+        public Country GetCountry(string url)
+        {
+            string response = GetStringFromUrl(url);
+            if (mode == 0)
+            {
+                response = EditStringFromRestcountries(response);
+            }
+            if (mode == 1)
+            {
+                response = EditStringFromHtmlweb(response);
+            }
+            if (mode == 2)
+            {
+                response = EditStringFromIpgeolocationapi(response);
+            }
+
+            return DeserializeJSON(response);
+            
+        }
+
+        private string GetStringFromUrl(string url)
         {
             var webClient = new WebClient();
             try
             {
                 var response = webClient.DownloadString(url);
-                if (mode == 0)
-                {
-                    response = response.Substring(1, response.Length - 2);
-                }
-                if (mode == 1)
-                {
-                    response = response.Replace("\"country\":{", "");
-                    response = response.Replace("},\"english", ",\"english");
-                    response = response.Replace("location", "region");
-                    response = response.Replace("iso", "numericCode");
-                    response = response.Replace("\\/", "/");
-                    response = response.Substring(0, response.Length - 1) + ",\"area\":0.0}";
-                    response = response.Substring(0, response.Length - 1) + ",\"population\":0}";
-                    response = response.Substring(0, response.Length - 1) + ",\"capital\":\"none\"}";
-
-                }
-                if (mode == 2)
-                {
-                    response = response.Substring(0, response.Length - 1) + ",\"area\":0.0}";
-                    response = response.Substring(0, response.Length - 1) + ",\"population\":0}";
-                    response = response.Substring(0, response.Length - 1) + ",\"capital\":\"unknown\"}";
-                    response = response.Replace("number", "numericCode");
-                }
-                response = response.Replace("'", "*");
-                response = response.Replace("\"", "'");
-                Country country = JsonConvert.DeserializeObject<Country>(response);
-                return country;
+                return response;
             }
             catch (WebException ex)
             {
@@ -53,6 +47,39 @@ namespace CountryInfo
             {
                 throw;// new Exception("Полученный формат не является JSON-объектом.", ex);
             }
+        }
+        private string EditStringFromRestcountries(string response)
+        {
+            return response = response.Substring(1, response.Length - 2);
+        }
+        private string EditStringFromHtmlweb(string response)
+        {
+
+            response = response.Replace("\"country\":{", "");
+            response = response.Replace("},\"english", ",\"english");
+            response = response.Replace("location", "region");
+            response = response.Replace("iso", "numericCode");
+            response = response.Replace("\\/", "/");
+            response = response.Substring(0, response.Length - 1) + ",\"area\":0.0}";
+            response = response.Substring(0, response.Length - 1) + ",\"population\":0}";
+            response = response.Substring(0, response.Length - 1) + ",\"capital\":\"none\"}";
+            return response;
+        }
+        private string EditStringFromIpgeolocationapi(string response)
+        {
+            response = response.Substring(0, response.Length - 1) + ",\"area\":0.0}";
+            response = response.Substring(0, response.Length - 1) + ",\"population\":0}";
+            response = response.Substring(0, response.Length - 1) + ",\"capital\":\"unknown\"}";
+            response = response.Replace("number", "numericCode");
+            return response;
+        }
+
+        private Country DeserializeJSON(string response)
+        {
+            response = response.Replace("'", "*");//избавляемся от ' в названиях
+            response = response.Replace("\"", "'");//заменяем двойные кавычки на одинарные
+            Country country = JsonConvert.DeserializeObject<Country>(response);
+            return country;
         }
     }
 }
